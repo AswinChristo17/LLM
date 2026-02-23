f804c38f78967b7581402d089f3a0426e0732896
Aswin Christo <aswinchristo17@gmail.com>
2026-02-23 10:16:05 +0530

    Add files via upload

---
+++ test_docs.js
@@ -0,0 +1,17 @@
+{
+  "name": "prompt-injection-lab",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "index_vulnerable.js",
+  "scripts": {
+    "vuln": "node index_vulnerable.js",
+    "secure": "node index_hardened.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "dotenv": "^17.3.1"
+  }
+}
